7a59443fd26e84211de4adf1f345dc0f9b5d77ec
aryan30640 <aryan30640@gmail.com>
2024-11-16 01:32:58 +0530

    Updated

---
+++ Table.py
@@ -0,0 +1,8 @@
+print("Enter a Number to get it's Table:")
+#Taking input from User.
+n = int(input())
+print("the table of", n, "is:")
+#Initializing loop.
+#If 'n' is the last term in loop then range ends with n + 1.
+for i in range(1, 11):
+    print(n*i)
