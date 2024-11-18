0fa3ac40b1fa3bb2f6c45a7664ccca3cfac665dc
aryan30640 <aryan30640@gmail.com>
2024-11-19 03:23:32 +0530

    Updated

---
+++ marks.py
@@ -0,0 +1,13 @@
+marks = int(input("enter your marks:"))
+
+#and can be used here
+if (marks >= 90):
+    print("A")
+elif(marks >= 80):
+    print("B")
+elif(marks >= 80):
+    print("C")
+elif(marks >= 70):
+    print("D")
+else:
+    print("fail!")
--- redlight.py
+++ redlight.py
@@ -1,10 +1,10 @@
 color = input("input a color(r/g/y):")
 
-if(color == "r"):
+if color == "r":
     print("stop")
-elif(color == "g"):
+elif color == "g":
     print("you can go")
-elif(color == "y"):
+elif color == "y":
     print("wait")
 else:
     print("Syntax Error")
