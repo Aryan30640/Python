8fe24ae7e9513122778b3d4806faed1a75d568d5
aryan30640 <aryan30640@gmail.com>
2024-11-25 12:21:51 +0530

    Updated

--- input.py
+++ input.py
@@ -1,5 +1,8 @@
-name = input("Enter your name:")
+name = input("Enter your name: ")
 print(name, "has", len(name), "letter")
 
-enter = input("Enter a text:")
+enter = input("Enter a text: ")
 print("text has", enter.count("$"), "$")
+
+water = input("please enter a integer, string or any type of value: ")
+print("above text has", water.count("a"), "a(s).")
